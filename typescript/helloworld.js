2fbc8bd6d96abc44146cba4987ead4e3fa7cdc4f
Jesper Hasselquist <jesper.hasselquist@gmail.com>
2021-04-12 23:10:15 +0900

    Add ts quirk

--- typescript/helloworld.js
+++ typescript/helloworld.js
@@ -1,3 +1,17 @@
 var message = "Hello World!";
 console.log(message);
-//# sourceMappingURL=helloworld.js.map
+var Derp = /** @class */ (function () {
+    function Derp() {
+    }
+    Object.defineProperty(Derp.prototype, "prop", {
+        // This prevents treeshaking because transpiling to Object.defineProperty
+        // https://mtjody.vercel.app/posts/tree-shaking-ts
+        get: function () {
+            return 69;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return Derp;
+}());
+;
--- typescript/helloworld.ts
+++ typescript/helloworld.ts
@@ -1,2 +1,11 @@
 let message: string = "Hello World!";
 console.log(message);
+
+class Derp {
+    // This prevents treeshaking because transpiling to Object.defineProperty
+    // https://mtjody.vercel.app/posts/tree-shaking-ts
+    get prop() {
+        return 69;
+    }
+};
+
