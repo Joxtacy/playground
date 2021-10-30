db3bffacec0598e1f7125254887cbb1e55630523
Jesper Hasselquist <jesper.hasselquist@gmail.com>
2021-10-30 15:06:21 +0900

    Add enums

---
+++ the_rust_programming_language/enums/src/main.rs
@@ -0,0 +1,64 @@
+fn main() {
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+
+    route(four);
+    route(six);
+
+    #[derive(Debug)]
+    struct IpAddrDumb {
+        kind: IpAddrKind,
+        address: String,
+    }
+
+    let home = IpAddrDumb {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let loopback = IpAddrDumb {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+
+    dbg!(&home);
+    dbg!(&loopback);
+
+    // More concise version of the above
+    // Enums can have different variants. structs are all the same
+    enum IpAddr {
+        V4(u8, u8, u8, u8),
+        V6(String),
+    }
+
+    let home = IpAddr::V4(127, 0, 0, 1);
+
+    let loopback = IpAddr::V6(String::from("::1"));
+
+    // Another enum example
+    #[derive(Debug)]
+    enum Message {
+        Quit,
+        Move { x: i32, y: i32 },
+        Write(String),
+        ChangeColor(i32, i32, i32),
+    }
+
+    impl Message {
+        fn call(&self) {
+            // method body would be defined here
+            dbg!(&self);
+        }
+    }
+
+    let m = Message::Write(String::from("hello"));
+    m.call();
+}
+
+#[derive(Debug)]
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
+fn route(_ip_kind: IpAddrKind) {}
