b0f609d1dbb0e31f92f2dce29ad5dd0c50997f5b
Jesper Hasselquist <jesper.hasselquist@gmail.com>
2022-02-26 11:39:33 +0900

    svelte-nodegui-starter

---
+++ svelte-nodegui-starter/src/components/StepOne.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    const dogImg = require("./dog");
+    const styling = `
+    margin-top: 8px;
+    margin-horizontal: 16px;
+    padding-horizontal: 8px;
+    `;
+</script>
+
+<view style={styling}>
+    <text wordWrap={true}>
+        Edit App.svelte to make changes to this screen. Then come back to see your changes. The app will restart with each change, thanks to Live Reload 🔥
+    </text>
+    <text>
+      {`
+        <p style="color: rgb(255,72,38);">
+          <center>
+            <img src="${dogImg}" alt="doggy" />
+          </center>
+          <center>You can even use <i><strong>rich HTML text</strong></i> like this if you want 😎</center>
+        </p>
+        <hr />
+      `}
+    </text>
+</view>
