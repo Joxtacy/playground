b28a65d1372dd577e7226d72ef5594f5c4aa303e
Jesper Hasselquist <jesper.hasselquist@gmail.com>
2022-05-07 21:03:20 +0900

    svelte-cubed-stuff: add overlap and cutting

--- svelte-cubed-stuff/src/routes/stacked/index.svelte
+++ svelte-cubed-stuff/src/routes/stacked/index.svelte
@@ -7,19 +7,15 @@
     threejs: THREE.Mesh;
     width: number;
     depth: number;
-    direction?: "x" | "z";
+    direction: "x" | "z";
   }
 
   const boxHeight = 1;
   const originalBoxSize = 3;
 
-  const geometry = new THREE.BoxGeometry(originalBoxSize, boxHeight, originalBoxSize);
-  const material = new THREE.MeshLambertMaterial({ color: 0xfb8e00 });
-  const mesh = new THREE.Mesh(geometry, material);
-  mesh.position.set(0, 0, 0);
-
   let gameStarted = false;
   let stack: GeneratedBox[] = [];
+  let overhangs: GeneratedBox[] = [];
 
   let cameraYPos = 4;
   let targetY = 0;
@@ -51,11 +47,12 @@
       threejs: mesh,
       width,
       depth,
+      direction: "x",
     };
   };
 
   const addLayer = (x: number, z: number, width: number, depth: number, direction: "x" | "z") => {
-    const y = boxHeight * stack.length;
+    const y = boxHeight * stack.length; // Add the new box one layer higher
 
     const layer = generateBox(x, y, z, width, depth);
     layer.direction = direction;
@@ -63,22 +60,69 @@
     stack = [...stack, layer];
   };
 
+  const addOverhang = (x: number, z: number, width: number, depth: number) => {
+    const y = boxHeight * (stack.length - 1); // Add the new box on the same layer
+    const overhang = generateBox(x, y, z, width, depth);
+
+    overhangs = [...overhangs, overhang];
+  };
+
   const onClick = () => {
     if (!gameStarted) {
       // renderer.setAnimationLoop(animation)
       gameStarted = true;
     } else {
-      const topLayer = stack[stack.length - 1];
+      const topLayer = stack.splice(stack.length - 1)[0];
+      const previousLayer = stack[stack.length - 1];
+
       const direction = topLayer.direction;
 
-      // Next layer
-      const nextX = direction === "x" ? 0 : -10;
-      const nextZ = direction === "z" ? 0 : -10;
-      const newWidth = originalBoxSize;
-      const newDepth = originalBoxSize;
-      const nextDirection = direction === "x" ? "z" : "x";
+      const delta =
+        topLayer.threejs.position[direction] - previousLayer.threejs.position[direction];
 
-      addLayer(nextX, nextZ, newWidth, newDepth, nextDirection);
+      const overhangSize = Math.abs(delta);
+
+      const size = direction === "x" ? topLayer.width : topLayer.depth;
+
+      const overlap = size - overhangSize;
+
+      if (overlap > 0) {
+        // Cut layer
+        const newWidth = direction === "x" ? overlap : topLayer.width;
+        const newDepth = direction === "z" ? overlap : topLayer.depth;
+
+        // Update metadata
+        topLayer.width = newWidth;
+        topLayer.depth = newDepth;
+
+        // Update ThreeJS model
+        topLayer.threejs.scale[direction] = overlap / size;
+        topLayer.threejs.position[direction] -= delta / 2;
+
+        stack = [...stack, topLayer];
+
+        // Overhang
+        const overhangShift = (overlap / 2 + overhangSize / 2) * Math.sign(delta);
+        const overhangX =
+          direction === "x"
+            ? topLayer.threejs.position.x + overhangShift
+            : topLayer.threejs.position.x;
+        const overhangZ =
+          direction === "z"
+            ? topLayer.threejs.position.z + overhangShift
+            : topLayer.threejs.position.z;
+        const overhangWidth = direction === "x" ? overhangSize : newWidth;
+        const overhangDepth = direction === "z" ? overhangSize : newDepth;
+
+        addOverhang(overhangX, overhangZ, overhangWidth, overhangDepth);
+
+        // Next layer
+        const nextX = direction === "x" ? topLayer.threejs.position.x : -10;
+        const nextZ = direction === "z" ? topLayer.threejs.position.z : -10;
+        const nextDirection = direction === "x" ? "z" : "x";
+
+        addLayer(nextX, nextZ, newWidth, newDepth, nextDirection);
+      }
     }
   };
 
@@ -113,6 +157,14 @@
       geometry={box.threejs.geometry}
       material={box.threejs.material}
       position={box.threejs.position}
+      scale={box.threejs.scale}
+    />
+  {/each}
+  {#each overhangs as box}
+    <SC.Mesh
+      geometry={box.threejs.geometry}
+      material={box.threejs.material}
+      position={box.threejs.position}
     />
   {/each}
   <SC.AmbientLight intensity={0.6} />
@@ -127,5 +179,5 @@
     position={[4, cameraYPos, 4]}
     target={[0, targetY, 0]}
   />
-  <SC.OrbitControls />
+  <!-- <SC.OrbitControls /> -->
 </SC.Canvas>
